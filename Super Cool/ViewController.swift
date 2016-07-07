67134703f2f11d086a26393800b9c3c2268e1e90
Mini <Dip3@Minis-Mac-mini.local>
2016-07-07 02:31:29 -0400

    added files for SuperCool

--- Super Cool/ViewController.swift
+++ Super Cool/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var uncoolButton: UIButton!
+    @IBOutlet weak var coolButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        coolButton.backgroundColor = UIColor.clearColor()
+        coolButton.layer.cornerRadius = 5
+        coolButton.layer.borderWidth = 1
+        coolButton.layer.borderColor = UIColor.blueColor().CGColor
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +32,19 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        coolButton.hidden = true
+        uncoolButton.hidden = false
+    }
+    
+    @IBAction func makeMeUncool(sender: AnyObject) {
+        coolLogo.hidden = true
+        coolBg.hidden = true
+        uncoolButton.hidden = true
+        coolButton.hidden = false;
+    }
 }
 
